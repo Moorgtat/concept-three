6e678cfcd1dad2f8a67953f14eb7a08373475c36
Moorgtat <mat.lecardinal@gmail.com>
2020-02-26 23:13:21 +0100

    [server] Modif Archi directory.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hello World!')
+});
+
+router.get('/users', function(req, res, next) {
+  res.send('Hello users!')
 });
 
 module.exports = router;
