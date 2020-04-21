efb6ad1a6383bc3f00ae4034adcf1ab60b16e815
Moorgtat <mat.lecardinal@gmail.com>
2020-04-21 22:49:40 +0200

    [concept-three] Ls Cart Vuex Ref.

--- client/src/store/cart.js
+++ client/src/store/cart.js
@@ -1,10 +1,20 @@
 export const cart = {
   state: {
     cart: JSON.parse(localStorage.getItem('cloneChamber')),
+    cartPrice: '',
+    cartItemNumber: '',
     item: {}
   },
   actions: {
+    addItem () {},
+    minorItem () {},
+    deleteItem () {}
   },
   mutations: {
+    setcart () {
+      const parsed = JSON.stringify(this.state.cart)
+      localStorage.setItem('cloneChamber', parsed)
+      this.cloneChamber = JSON.parse(localStorage.getItem('cloneChamber'))
+    }
   }
 }
